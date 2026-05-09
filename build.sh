3ec1d543a8dd16ca53d75a5279374f7fca075382
Leon OKOMBE <okombeleonrobland@gmail.com>
2026-05-09 19:30:22 +0200

    refactor: setup render.yaml for deploiment

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ poetry install --no-interaction --no-ansi --only main --no-root
 poetry run python  stem_learning_app/manage.py collectstatic --no-input
 
 poetry run python  stem_learning_app/manage.py migrate
+
+cd stem_learning_app/
