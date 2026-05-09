720636102cceaf36854fad90e355468ae7e49b49
Leon OKOMBE <okombeleonrobland@gmail.com>
2026-05-09 19:13:30 +0200

    refactor: setup render.yaml for deploiment

--- stem_learning_app/config/settings.py
+++ stem_learning_app/config/settings.py
@@ -216,6 +216,8 @@ if not USE_INTERNAL_STATIC:
     }
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
